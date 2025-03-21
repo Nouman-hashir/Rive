77187bb234c427f66b9ddd8006792581fcef13d0
Nouman-hashir <134682051+Nouman-hashir@users.noreply.github.com>
2025-03-21 15:27:30 +0500

    rive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -30,44 +32,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  StateMachineController? _controller;
+  SMIInput<double>? _xInput;
+  SMIInput<double>? _yInput;
 
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
+  // Store the size of the animation widget
+  Size _animationSize = Size.zero;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onRiveInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(
+      artboard,
+      'FaceTracking-StateMachine', // Replace with your State Machine name
+    );
+
+    if (controller != null) {
+      setState(() {
+        _controller = controller;
+        artboard.addController(controller);
+
+        _xInput = controller.findInput<double>('xPosition');
+        _yInput = controller.findInput<double>('yPosition');
+      });
+    }
+  }
+
+  // Handle mouse movement
+  void _updatePosition(Offset position) {
+    if (_xInput != null && _yInput != null) {
+      setState(() {
+        // Convert screen coordinates to animation coordinates
+        // You might need to adjust these calculations based on your needs
+        _xInput!.value = position.dx / _animationSize.width;
+        _yInput!.value = position.dy / _animationSize.height;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
       appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
+        backgroundColor: Color.fromARGB(255, 175, 173, 173),
+        title: Text('Rive Animations',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold)),
+        centerTitle: true,
       ),
       body: Center(
-      
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        child: MouseRegion(
+          onHover: (event) {
+            _updatePosition(event.localPosition);
+          },
+          child: SizedBox(
+            width: 400,
+            height: 400,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                _animationSize =
+                    Size(constraints.maxWidth, constraints.maxHeight);
+                return RiveAnimation.asset(
+                  'assets/michifacetracker (1).riv',
+                  fit: BoxFit.contain,
+                  onInit: _onRiveInit,
+                );
+              },
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
